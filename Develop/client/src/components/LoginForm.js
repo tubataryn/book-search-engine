8fd0376c32cf95b4cc46374679beb538cda1c8a2
Taryn Fanning <taryn.fanning@gmail.com>
2020-11-07 16:23:35 -0600

    content

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,14 +1,27 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
+
+import React, { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/react-hooks';
 import { loginUser } from '../utils/API';
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error])
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
